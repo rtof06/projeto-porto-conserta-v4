8206fd7700ce9288262f79481e8521104658f4c4
rtof06 <ricardoofilho06@gmail.com>
2024-11-04 23:11:49 -0300

    ROUTE ATUALIZADO

--- src/app/api/cadastro/route.ts
+++ src/app/api/cadastro/route.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function PUT(req: NextApiRequest, res: NextApiResponse) {
   const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
   if (req.method === "PUT") {
